95cdbd2a3add5d46e8f858de79f51fc4590a332d
Cicadezz <1262260557@qq.com>
2023-09-02 21:44:26 +0800

    第一次提交咯

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- 定义生成HTML代码 -->
+<template>
+  <div >
+    <!-- <h1>{{ message }}</h1> -->
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/element/ElementView.vue'
+export default {
+  components: { ElementView },
+  data() {
+    return {
+      message:"Hello Vue"
+    }
+  },
+}
+</script>
+
+<!-- 控制css样式 -->
+<style>
+h1{
+  color: brown;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
